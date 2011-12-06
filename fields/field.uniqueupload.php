32d949303837ab7defeef7bbc292e6eeacb28e33
Nils Werner <nils.werner@gmail.com>
2011-12-06 16:52:03 +0100

    make getUniqueFilename a static function

--- fields/field.uniqueupload.php
+++ fields/field.uniqueupload.php
@@ -10,19 +10,19 @@
 			$this->_name = __('Unique File Upload');
 		}
 
-		private function getUniqueFilename($filename) {
+		private static function getUniqueFilename($filename) {
 			## since uniqid() is 13 bytes, the unique filename will be limited to ($crop+1+13) characters;
 			$crop  = '30';
 			return preg_replace("/([^\/]*)(\.[^\.]+)$/e", "substr('$1', 0, $crop).'-'.uniqid().'$2'", $filename);
 		}
 
 		public function checkPostFieldData($data, &$message, $entry_id = NULL) {
-			if (is_array($data) and isset($data['name'])) $data['name'] = $this->getUniqueFilename($data['name']);
+			if (is_array($data) and isset($data['name'])) $data['name'] = self::getUniqueFilename($data['name']);
 			return parent::checkPostFieldData($data, $message, $entry_id);
 		}
 
 		public function processRawFieldData($data, &$status, $simulate = false, $entry_id = NULL) {
-			if (is_array($data) and isset($data['name'])) $data['name'] = $this->getUniqueFilename($data['name']);
+			if (is_array($data) and isset($data['name'])) $data['name'] = self::getUniqueFilename($data['name']);
 			return parent::processRawFieldData($data, $status, $simulate, $entry_id);
 		}
 	}
