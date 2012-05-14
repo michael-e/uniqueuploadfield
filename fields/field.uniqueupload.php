439ba36115f0425ae178ce5c975dacef7df9b60b
michael-e <info@michael-eichelsdoerfer.de>
2012-05-14 10:28:14 +0200

    Renamed 'original filename' to 'clean filename'.

--- fields/field.uniqueupload.php
+++ fields/field.uniqueupload.php
@@ -16,7 +16,7 @@
 			return preg_replace("/([^\/]*)(\.[^\.]+)$/e", "substr('$1', 0, $crop).'-'.uniqid().'$2'", $filename);
 		}
 
-		private static function getOriginalFilename($filename) {
+		private static function getCleanFilename($filename) {
 			return preg_replace("/([^\/]*)(\-[a-f0-9]{13})(\.[^\.]+)$/", '$1$3', $filename);
 		}
 
@@ -34,6 +34,6 @@
 			parent::appendFormattedElement($wrapper, $data);
 			$field = $wrapper->getChildrenByName($this->get('element_name'));
 			if(!empty($field))
-				end($field)->appendChild(new XMLElement('original-filename', General::sanitize(self::getOriginalFilename(basename($data['file'])))));
+				end($field)->appendChild(new XMLElement('clean-filename', General::sanitize(self::getCleanFilename(basename($data['file'])))));
 		}
 	}
