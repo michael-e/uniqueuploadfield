c0bc600f187c33c9e162405849028e0b7191f969
michael-e <info@michael-eichelsdoerfer.de>
2012-05-14 10:36:58 +0200

    Put static helper functions at the bottom.

--- fields/field.uniqueupload.php
+++ fields/field.uniqueupload.php
@@ -10,16 +10,6 @@
 			$this->_name = __('Unique File Upload');
 		}
 
-		private static function getUniqueFilename($filename) {
-			## since uniqid() is 13 bytes, the unique filename will be limited to ($crop+1+13) characters;
-			$crop  = '30';
-			return preg_replace("/([^\/]*)(\.[^\.]+)$/e", "substr('$1', 0, $crop).'-'.uniqid().'$2'", $filename);
-		}
-
-		private static function getCleanFilename($filename) {
-			return preg_replace("/([^\/]*)(\-[a-f0-9]{13})(\.[^\.]+)$/", '$1$3', $filename);
-		}
-
 		public function checkPostFieldData($data, &$message, $entry_id = NULL) {
 			if (is_array($data) and isset($data['name'])) $data['name'] = self::getUniqueFilename($data['name']);
 			return parent::checkPostFieldData($data, $message, $entry_id);
@@ -35,5 +25,15 @@
 			$field = $wrapper->getChildrenByName($this->get('element_name'));
 			if(!empty($field))
 				end($field)->appendChild(new XMLElement('clean-filename', General::sanitize(self::getCleanFilename(basename($data['file'])))));
+		}
+
+		private static function getUniqueFilename($filename) {
+			## since uniqid() is 13 bytes, the unique filename will be limited to ($crop+1+13) characters;
+			$crop  = '30';
+			return preg_replace("/([^\/]*)(\.[^\.]+)$/e", "substr('$1', 0, $crop).'-'.uniqid().'$2'", $filename);
+		}
+
+		private static function getCleanFilename($filename) {
+			return preg_replace("/([^\/]*)(\-[a-f0-9]{13})(\.[^\.]+)$/", '$1$3', $filename);
 		}
 	}
